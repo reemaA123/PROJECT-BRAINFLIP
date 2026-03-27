10d15706b34310bdf95db5e6c0f4a1b424dba52b
reema_arsenio <reemageminiano.arsenio@gmail.com>
2026-03-27 22:32:13 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>BrainFlip</title>
+    <base href="/" />
+    <meta name="viewport" content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="theme-color" content="#8b5cf6" />
+    <link rel="manifest" href="manifest.webmanifest" />
+    <script type="module" src="https://cdn.jsdelivr.net/npm/ionicons@7.0.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://cdn.jsdelivr.net/npm/ionicons@7.0.0/dist/ionicons/ionicons.js"></script>
+  </head>
+  <body>
+    <app-root></app-root>
+  </body>
+</html>
