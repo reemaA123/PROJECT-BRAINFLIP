a6a51c4a816e10eb13308b4f64318ef8a5f67cde
reema_arsenio <reemageminiano.arsenio@gmail.com>
2026-03-27 22:50:24 +0800

    Update app.component.spec.ts

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -1,3 +1,16 @@
-<ion-app>
-  <ion-router-outlet></ion-router-outlet>
-</ion-app>
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  it('should create the app', async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
+    
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
